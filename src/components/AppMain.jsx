c1c9ee482befec53777434471541d2ed974a6369
salvatore tullo <salvatoretullo15@gmail.com>
2026-03-12 17:58:11 +0100

    creazione della lista precompilata con aggiunta dei bottoni

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 export default function AppMain(){
     const [game, setGame] = useState("")
     const [Description, setDescription]= useState("")
+    const souls = ["dark souls 1","dark souls 2","dark souls 3","steelrising","the surge 1","the surge 2","bloodborne","elden ring"]
 
     function handleSubmit(e) {
       e.preventDefault()
@@ -13,11 +14,26 @@ export default function AppMain(){
     }
 
   return (
-    <main className="container d-flex justify-content-center"> 
-      <form className="" onSubmit={handleSubmit}>
-        <input type="text" value={game} onChange={e => setGame(e.target.value)}></input>
-        <button type="submit">add game</button>
-      </form>
+    <main> 
+    <div className="container">
+      <div className="card p-3">
+        <form onSubmit={handleSubmit}>
+          <div className='d-flex gap-3 mb-3 justify-content-center'>
+            <input type="text" value={game} onChange={e => setGame(e.target.value)}></input>
+            <button className="btn btn-dark text-danger" type="submit">ADD GAME</button>
+          </div>
+        </form>
+          <ul className="list-group">
+            {souls.map(soul =>(
+              <li className="list-group-item d-flex justify-content-between align-items-center p-3">
+              {soul}
+              <button className="badge text-bg-warning rounded-pill">🗑️😡</button>
+              </li>
+            ))}
+          </ul>
+      </div>
+    </div>
+    
     </main>
   )
 }
